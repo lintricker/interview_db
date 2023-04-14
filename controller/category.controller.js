29f33b3bca8fd2ecc89dd656c2af94347b6c325b
lintricker <alina.ishem01@gmail.com>
2023-04-14 16:55:14 +0400

    doneCategories

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -1,20 +1,31 @@
+const db = require('../db')
+
 class CategoryController {
     async createCategory(req, res) {
         const {title} = req.body
-        console.log(title)
-        res.json('ok')
+        const newCategory = await db.query(`INSERT INTO category (title) values ($1) RETURNING *`, [title])
+        res.json(newCategory.rows[0])
     }
     async getCategories(req, res) {
-
+        const categories = await db.query('SELECT * FROM category')
+        res.json(categories.rows)
     }
     async getOneCategory(req, res) {
-
+        const id = req.params.id
+        const category = await db.query('SELECT * FROM category where id = $1', [id])
+        res.json(category.rows[0])
     }
     async updateCategory(req, res) {
-
+        const {id, title} = req.body
+        const category = await db.query('UPDATE category set title = $1 where id = $2 RETURNING *',
+        [title, id]
+        )
+        res.json(category.rows[0])
     }
     async deleteCategory(req, res) {
-
+        const id = req.params.id
+        const category = await db.query('DELETE FROM category where id = $1', [id])
+        res.json(category.rows[0])
     }
 }
 
--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(express.json())
-app.use('api', categoryRouter)
+app.use('/api', categoryRouter)
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
