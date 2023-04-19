26292d51c8d1b58f7d9778cff35fd9bf7ea38605
lintricker <alina.ishem01@gmail.com>
2023-04-19 13:49:06 +0400

    question

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const categoryRouter = require('./routes/category.routes')
 const typeRouter = require('./routes/type.routes')
 const complexityRouter = require('./routes/complexity.routes')
+const questionRouter = require('./routes/question.routes')
 
 const PORT = process.env.PORT || 8080
 
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use('/api', categoryRouter)
 app.use('/api', typeRouter)
 app.use('/api', complexityRouter)
+app.use('/api', questionRouter)
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
