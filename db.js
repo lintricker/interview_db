34fb104ec328fb0c40c0bd38a6f1f48af0745bcb
lintricker <99593533+lintricker@users.noreply.github.com>
2023-10-22 01:18:25 +0400

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: "postgres",
-    password: "dbadmin",
+    password: "postgres",
     host: "localhost",
     port: 5432,
-    database: "interview_db"
+    database: "postgres"
 })
 
 
